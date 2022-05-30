8088860d1718a25a67eccdd516e79c80f00fe71e
a.muminov <abdulkhamid.mumin1@gmail.com>
2022-05-30 22:05:45 +0300

    lab1

---
+++ app_python/PYTHON.md
@@ -0,0 +1,31 @@
+# Best practices for writing Python apps
+
+1. Use `requirements.txt` file for Python packages.
+2. Use `.gitignore` file to exclude not relevant files from the git.
+3. Use virtual environment for each project to not mess all packages and their versions in the system.
+4. Use linters to fix little mistakes, stylistic inconsistencies, and dangerous logic:
+* [pylint](https://pylint.org)
+* [pyflakes](https://github.com/PyCQA/pyflakes)
+* [pycodestyle](https://github.com/PyCQA/pycodestyle)
+5. Use static analysis tools to easily detect vulnerabilities or some problems:
+* [pylint](https://pylint.org)
+* [pyflakes](https://github.com/PyCQA/pyflakes)
+* [prospector](https://prospector.landscape.io/en/master/)
+* [SonarQube](https://www.sonarqube.org)
+6. Use formatting tools:
+* [black](https://github.com/psf/black)
+* [isort](https://github.com/PyCQA/isort)
+
+# Best practices for writing Unit tests
+
+1. Unit tests are checking the functionality of one particular part of the program(e.g. function), so refactoring the code helps for unit testing.
+2. Make your unit test as small as possible, in a way that the minimum possible piece of code is involved. This way, when an error appears, you will be able to quickly assess where it originated.
+3. Treat test code as core code. Make your test code readable, use docstrings and comments and respect style, almost as if it were a part of the functional code base.
+4. Parametrize your tests. Don't use the same test copy-pasted many times with different inputs. With Pytest you can reduce it to only one test. 
+5. Try to use Test Driven Development in the project.
+
+## References:
+* https://luminousmen.com/post/python-static-analysis-tools
+* https://dzone.com/articles/7-best-python-code-review-tools-recommended-by-dev
+* https://medium.com/worldsensing-techblog/tips-and-tricks-for-unit-tests-b35af5ba79b1
+* https://realpython.com/python-code-quality/
---
+++ app_python/moscow_time/main.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+import json
+import requests
+from fastapi import FastAPI
+from os import path
+from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_time():
+    """Make GET request to WorldTimeAPI about current Moscow Time and return it.
+
+    Returns
+    -------
+    html
+        an html page with current Moscow time or with error code.
+    """
+    style = """
+        \"background-image: url('https://wallpaperaccess.com/full/372581.jpg');
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        flex-direction: column;
+        color: white;
+        font-size: 30px;\"
+    """
+    html_content = """
+        <html>
+            <head>
+                <title>Moscow Current Time</title>
+            </head>
+            <body style=%s>
+                <h1>Moscow time</h1>
+                <h1>%s</h1>
+            </body>
+        </html>
+        """
+    url = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
+    response = requests.get(url)
+    if response.status_code == 200:
+        pattern = "%Y-%m-%dT%H:%M:%S.%f%z"
+        date = datetime.strptime(response.json()["datetime"], pattern)
+        time = f"{date.hour}:{date.minute}:{date.second}"
+        await write_time(time)
+        return HTMLResponse(content=html_content % (style, time),
+                            status_code=200)
+    else:
+        error = f"Error: {response.status_code}"
+        return HTMLResponse(content=html_content % (style, error),
+                            status_code=404)
+
+@app.get("/visits")
+async def get_visits():
+    if not path.exists('data/visits.json'):
+        with open('visits.json', 'w') as f:
+            pass
+    def iterfile():
+        with open("data/visits.json", mode="r") as file:
+            yield from file
+    return StreamingResponse(iterfile())
