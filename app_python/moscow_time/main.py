ced2c5693d4f64c35914de60fe60724ffe6d86d6
a.muminov <abdulkhamid.mumin1@gmail.com>
2022-06-19 16:53:44 +0300

    fix/logic: chisto po fanu

--- app_python/moscow_time/main.py
+++ app_python/moscow_time/main.py
@@ -67,17 +67,17 @@ async def get_time():
 
 @app.get("/visits")
 async def get_visits():
-    if not path.exists('data/visits.json'):
+    if not path.exists('app_python/data/visits.json'):
         with open('visits.json', 'w') as f:
             pass
 
     def iterfile():
-        with open("data/visits.json", mode="r") as file:
+        with open("app_python/data/visits.json", mode="r") as file:
             yield from file
 
     return StreamingResponse(iterfile())
 
 
 async def write_time(time):
-    with open("labs/data/visits.json", "a") as file:
+    with open("app_python/data/visits.json", "a") as file:
         file.write(f"Accessed at: {time}\n")
